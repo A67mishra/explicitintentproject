81ab4eab8475b3870b5d6bdebacc6f0a6c22b386
Ayush mishra <ampandit.2140@gmail.com>
2022-12-09 11:08:10 +0530

    my first commit

---
+++ app/src/main/java/com/example/cep1/Thirdactivity.java
@@ -0,0 +1,14 @@
+package com.example.cep1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Thirdactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thirdactivity);
+    }
+}
